ff29dc8e62e4afe1e7644ea1b1e63c90b86a8d4f
蔡金锋 <jeff.cai@99bill.com>
2021-12-09 15:46:55 +0800

    chore: ts infer

---
+++ test-typescript-handbook/infer.ts
@@ -0,0 +1,16 @@
+// ref: https://blog.csdn.net/KlausLily/article/details/108878205
+// ref: https://www.e-learn.cn/topic/3752913
+// ref: https://blog.csdn.net/KlausLily/article/details/108878205
+
+// https://juejin.cn/post/6850418120616738823
+
+function greetingA<T extends any[]>(x: T extends (infer R)[] ? R[] : T): T extends (infer R)[] ? R : T {
+  const [c] = x;
+  return c;
+
+  // return x
+}
+
+function greetingB() {
+  return greetingA([1]);
+}
