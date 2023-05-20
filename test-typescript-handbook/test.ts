e32a301f3b07bb4ea4c43227825fd5f8bd10a832
caijf <369756941@qq.com>
2023-05-20 21:12:29 +0800

    chore: 类型调整

--- test-typescript-handbook/test.ts
+++ test-typescript-handbook/test.ts
@@ -1,22 +1,22 @@
 // 交换字段名
-type ExchangeFieldNames<D extends any, F extends Record<string, keyof D>> = Omit<D, F[keyof F]> & { [P in keyof F]: D[F[P]] }
-
+type ExchangeFieldNames<D extends any, F extends Record<string, keyof D>> = Omit<D, F[keyof F]> & {
+  [P in keyof F]: D[F[P]];
+};
 
 // 交换字段名，支持嵌套
-// 先排除子级字段名，再交换字段名，然后加上子级字段名，再替换一次。这里的 F 有类型问题，因为排除了子级字段名，暂时没有比较好的方案处理。
+// 先排除子级字段名，再交换字段名，然后加上子级字段名，再替换一次。
 type TransformFieldNames<D extends any, F extends Record<string, any>, C extends string> = (C extends keyof D ? ExchangeFieldNames<Omit<D, C> & Record<C, TransformFieldNames<D, F, C>>, F> : ExchangeFieldNames<D, F>)[];
 
 // 测试
 // function transformFieldNames<D extends any, F extends Record<string, keyof D>, C extends string>(data: D[], fieldNames: F, childrenField?: C): TransformFieldNames<D, F, C> {
 //   // console.log(data, fieldNames, childrenField);
-//   return {} as any;
+//   return [];
 // }
 
 // 转换字段名方法
 // function transformFieldNames<D extends any, F extends Record<string, keyof D>, C extends string>(data: D[], fieldNames: F, childrenField?: C) {
 //   console.log(data, fieldNames, childrenField);
 
-//   // @ts-ignore
 //   type Option = C extends keyof D ? ExchangeFieldNames<Omit<D, C> & Record<C, Option[]>, F> : ExchangeFieldNames<D, F>;
 //   const r: Option[] = [];
 //   return r;
@@ -24,8 +24,8 @@ type TransformFieldNames<D extends any, F extends Record<string, any>, C extends
 
 // const options = [{ code: '1', name: 'one' }, { code: '2', name: 'two', children: [{ code: '2-1', name: 'two-one' }] }];
 
-// const t1 = transformFieldNames(options, { label: 'code', child: 'children' });
-// console.log(t1[1].name);
+// const t1 = transformFieldNames(options, { label: 'code', child: 'children' }, 'children');
+// console.log(t1[1]);
 // console.log(t1[0].child?.[0].code);
 
 // const t2 = transformFieldNames(options, { label: 'code', child: 'children' }, 'children');
@@ -33,11 +33,11 @@ type TransformFieldNames<D extends any, F extends Record<string, any>, C extends
 // console.log(t2[0].child[0].child[0].label);
 
 
-// 判断字段是否包含在对象中
-function contain<T extends object, S extends string>(obj: T, str: S): S extends keyof T ? string : number {
-  return (str in obj ? '1' : 1) as any;
-}
-var x = contain({ a: 1 }, 'a'); // string
-var y = contain({ b: 1 }, 'a'); // number
+// // 判断字段是否包含在对象中
+// function contain<T extends object, S extends string>(obj: T, str: S): S extends keyof T ? string : number {
+//   return (str in obj ? '1' : 1) as any;
+// }
+// var x = contain({ a: 1 }, 'a'); // string
+// var y = contain({ b: 1 }, 'a'); // number
 
 
