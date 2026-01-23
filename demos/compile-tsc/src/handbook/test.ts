41772ea504b2d1aa69bfad61d10c0568c8199b62
caijf <369756941@qq.com>
2026-01-23 14:58:16 +0800

    feat: ts示例前置的不定量参数

--- demos/compile-tsc/src/handbook/test.ts
+++ demos/compile-tsc/src/handbook/test.ts
@@ -1,35 +1,40 @@
 /* eslint-disable */
-// 交换字段名
-type ExchangeFieldNames<D extends any, F extends Record<string, keyof D>> = Omit<D, F[keyof F]> & {
+// -----交换字段名
+type ExchangeFieldNames<D, F extends Record<string, keyof D>> = Omit<D, F[keyof F]> & {
   [P in keyof F]: D[F[P]];
 };
 
 // 交换字段名，支持嵌套
 // 先排除子级字段名，再交换字段名，然后加上子级字段名，再替换一次。
-type TransformFieldNames<
-  D extends any,
-  F extends Record<string, any>,
-  C extends string
-> = (C extends keyof D
+type TransformFieldNames<D, F extends Record<string, any>, C extends string> = (C extends keyof D
   ? ExchangeFieldNames<Omit<D, C> & Record<C, TransformFieldNames<D, F, C>>, F>
   : ExchangeFieldNames<D, F>)[];
 
 // 测试
-// function transformFieldNames<D extends any, F extends Record<string, keyof D>, C extends string>(data: D[], fieldNames: F, childrenField?: C): TransformFieldNames<D, F, C> {
+// function transformFieldNames<D, F extends Record<string, keyof D>, C extends string>(data: D[], fieldNames: F, childrenField?: C): TransformFieldNames<D, F, C> {
 //   // console.log(data, fieldNames, childrenField);
 //   return [];
 // }
 
 // 转换字段名方法
-// function transformFieldNames<D extends any, F extends Record<string, keyof D>, C extends string>(data: D[], fieldNames: F, childrenField?: C) {
+// function transformFieldNames<D, F extends Record<string, keyof D>, C extends string>(
+//   data: D[],
+//   fieldNames: F,
+//   childrenField?: C
+// ) {
 //   console.log(data, fieldNames, childrenField);
 
-//   type Option = C extends keyof D ? ExchangeFieldNames<Omit<D, C> & Record<C, Option[]>, F> : ExchangeFieldNames<D, F>;
+//   type Option = C extends keyof D
+//     ? ExchangeFieldNames<Omit<D, C> & Record<C, Option[]>, F>
+//     : ExchangeFieldNames<D, F>;
 //   const r: Option[] = [];
 //   return r;
 // }
 
-// const options = [{ code: '1', name: 'one' }, { code: '2', name: 'two', children: [{ code: '2-1', name: 'two-one' }] }];
+// const options = [
+//   { code: '1', name: 'one' },
+//   { code: '2', name: 'two', children: [{ code: '2-1', name: 'two-one' }] }
+// ];
 
 // const t1 = transformFieldNames(options, { label: 'code', child: 'children' }, 'children');
 // console.log(t1[1]);
@@ -43,8 +48,8 @@ type TransformFieldNames<
 // function contain<T extends object, S extends string>(obj: T, str: S): S extends keyof T ? string : number {
 //   return (str in obj ? '1' : 1) as any;
 // }
-// var x = contain({ a: 1 }, 'a'); // string
-// var y = contain({ b: 1 }, 'a'); // number
+// const x = contain({ a: 1 }, 'a'); // string
+// const y = contain({ b: 1 }, 'a'); // number
 
 // 用于标识字段必填，但是值可以为undefined
 type RequiredKey<T> = {
@@ -52,3 +57,46 @@ type RequiredKey<T> = {
 };
 // 测试 RequiredKey
 type TestRequiredKey = RequiredKey<{ a: number; b: string }>;
+
+// -----部分可选
+type Optional<T, K extends keyof T> = Omit<T, K> & Partial<Pick<T, K>>;
+
+// test Optional
+// type OptionalObj = {
+//   a: 1;
+//   b: 'foo';
+// };
+// function testOptional(obj: Optional<OptionalObj, 'a'>) {
+//   if (obj.a) {
+//   }
+//   if (obj.b) {
+//   }
+// }
+
+// ----- 前置的不定量参数
+
+type JSTypeMap = {
+  string: string;
+  number: number;
+  boolean: boolean;
+  object: object;
+  array: any[];
+  function: (...args: any[]) => any;
+  symbol: symbol;
+  undefined: undefined;
+  null: null;
+};
+
+type JSTypeNames = keyof JSTypeMap;
+
+type ArgsType<T extends JSTypeNames[]> = {
+  [I in keyof T]: JSTypeMap[T[I]];
+};
+
+declare function addTepl<T extends JSTypeNames[]>(
+  ...args: [...T, (...args: ArgsType<T>) => void]
+): void;
+
+addTepl('string', 'number', 'boolean', (a, b, c) => {
+  console.log(a, b, c);
+});
